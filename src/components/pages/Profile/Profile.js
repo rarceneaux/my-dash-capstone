70b599d07356f9d36a3ff50ea5782775486bfb6a
Raymond Arceneaux <raymond.techcoder@gmail.com>
2020-02-02 02:00:18 -0600

    added comments

--- src/components/pages/Profile/Profile.js
+++ src/components/pages/Profile/Profile.js
@@ -3,6 +3,36 @@ import { Link } from 'react-router-dom';
 import './Profile.scss';
 
 class Profile extends React.Component {
+  componentDidMount() {
+    // eslint-disable-next-line func-names
+    window.fbAsyncInit = function () {
+      window.FB.init({
+        appId: 3584356521639073,
+        autoLogAppEvents: true,
+        xfbml: true,
+        version: 'v3.0',
+      });
+    };
+
+    // Load the SDK asynchronously
+    (function (d, s, id) {
+      let js;
+      const fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      // eslint-disable-next-line prefer-const
+      js = d.createElement(s);
+      js.id = id;
+      js.src = '//connect.facebook.net/en_US/sdk.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+    window.FB.XFBML.parse();
+  }
+
+  // componentDidUpdate() {
+  //   window.FB.XFBML.parse();
+  // }
+
+
   render() {
     const { userObj } = this.props;
     return (
@@ -12,9 +42,10 @@ class Profile extends React.Component {
   <h2 className="card-title ray">Welcome, { userObj.displayName} to your My-Dash Journal!</h2>
   <img src={userObj.photoURL} className="rounded-circle pic" alt="..."/>
   <div className="card-body">
-    <h5 className="login-time">You Last Login: {userObj.metadata.lastSignInTime}</h5>
   <Link className="btn btn-dark create" to={'/event/new'}>Add-Dash</Link>
   <Link className="btn btn-dark view" to={'/event'}>View-Dash</Link>
+    <h3 className="login-time">You Last Login: {userObj.metadata.lastSignInTime}</h3>
+  <div className="fb-comments" data-href="https://my-dash-93e26.firebaseapp.com/" data-width="500" data-numposts="10"></div>
   </div>
 </div>
   </div>
